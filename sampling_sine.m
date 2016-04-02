e2962b0742eea28d07447f467073c4082c816fb2
Kshtitij Purwar <kshitij.purwar4@gmail.com>
2016-04-02 18:41:31 +0530

    Added a sine fft demo and sampling demo

--- sampling_sine.m
+++ sampling_sine.m
@@ -1,16 +1,47 @@
-f=1000; %signal frequency
-fs=8000; %sampling frequency
-t=0:1/fs:1;
+f=1000;
+%signal frequency
+fs=5e5;
+%Initially oversampled to produce a smooth plot
+nCyl=5;
+t=0:1/fs:nCyl*1/f;
 y=sin(2*pi*f*t);
-subplot(2,1,1)
-plot(t,y,'r');
-axis([0 6e-3 -1.2 1.2]);
-sound(y,fs);
+subplot(3,1,1);
+plot(t,y,'b');
+title('Continuous sinusoidal signal of frequency=1000Hz');
+xlabel('Time(sec)');
+ylabel('Amplitude Y(t)');
+axis([0 nCyl/f -1.1 1.1])
 
-fs1=1500; %sampling frequency
-t=0:1/fs1:1;
-y1=sin(2*pi*f*t);
-subplot(2,1,2)
-plot(t,y1,'r');
-axis([0 6e-3 -1.2 1.2]);
-sound(y,fs);
+%Undersampling
+fs1=1500;
+t1=0:1/fs1:nCyl*1/f;
+y1=sin(2*pi*f*t1);
+
+%OverSampling
+fs2=4000;
+t2=0:1/fs2:nCyl*1/f;
+y2=sin(2*pi*f*t2);
+
+subplot(3,1,2)
+plot(t,y,'r');
+hold on;
+plot(t1,y1,':');
+hold on;
+stem(t1,y1,'b','filled')
+title('Continuous sinusoidal signal sampled at 1500Hz < Nq i.e. 2000Hz (If you look carefully there seem 2.5 cycles instead of 5) ');
+xlabel('Time(sec)');
+ylabel('Amplitude Y(t)');
+axis([0 nCyl/f -1.1 1.1])
+
+subplot(3,1,3)
+plot(t,y,'r');
+hold on;
+plot(t2,y2,':');
+hold on;
+stem(t2,y2,'b','filled')
+title('Continuous sinusoidal signal sampled at 4000Hz > Nq ');
+xlabel('Time(sec)');
+ylabel('Amplitude Y(t)');
+axis([0 nCyl/f -1.1 1.1]);
+
+print('sampling','-dpng'); %Use this to print the png of the graph plotted.
---
+++ sinefourier.m
@@ -0,0 +1,52 @@
+f1=10;
+f2=15;
+sampleFactor=30;
+fs=sampleFactor*f;
+phase=pi/3;
+nCyl=5;
+t=0:1/fs:1;
+
+x=sin(2*pi*f1*t +phase)+sin(2*pi*f2*t);
+subplot(2,2,1);
+plot(t,x,'LineSmoothing','on');
+title('X(t) is sin(2\pi15t) + sin(2\pi10t) ');
+xlabel('time (sec)');
+ylabel(' Amplitude  X(t)');
+mx=max(x);
+axis([0 1 -1.2*mx 1.2*mx]);
+
+NFFT=1024;
+X=fftshift(fft(x,NFFT));
+fVals=fs*(-NFFT/2:NFFT/2-1)/NFFT;
+subplot(2,2,2);
+plot(fVals,X,'LineSmoothing','on','LineWidth',1);
+title('Double Sided FFT with FFTshift');
+ylabel('|DFT values|');
+xlabel('Frequency (Hz)');
+f=max(f1,f2)+10;
+mX=1.1*abs(max(X));
+axis([-f f -10 mX]);
+
+L=length(x);
+Px=X.*conj(X)/(NFFT*L);
+subplot(2,2,3);
+plot(fVals,Px,'LineSmoothing','on','LineWidth',1);
+title('Power Spectral Density');
+ylabel('Power');
+xlabel('Frequency (Hz)');
+
+axis([-f f 0 mp]);
+
+
+
+sVals=fs*(0:NFFT/2-1)/NFFT;
+subplot(2,2,4);
+plot(fVals,Px,'LineSmoothing','on','LineWidth',1);
+title('One Sided Power Spectral Density');
+ylabel('Power spectral density');
+xlabel('Frequency (Hz)');
+axis([0 10+f  0 mp]);
+
+print('sinefft','-dpng');
+%http://www.gaussianwaves.com/2014/07/how-to-plot-fft-using-matlab-fft-of-b
+%asic-signals-sine-and-cosine-waves/
