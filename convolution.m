2fd21a2a78e08eba2b4954b8353ef3507dc646b6
Kshtitij Purwar <kshitij.purwar4@gmail.com>
2016-04-03 00:26:25 +0530

    modified convolutions to show the sequence using title({'blah','bla-blah'})

--- convolution.m
+++ convolution.m
@@ -20,7 +20,7 @@ ylabel('Y(n)');
 subplot(3,2,5);
 stem(lc,'filled');
 xlabel('n i.e. sample number');
-title('Linear Convolution of Sequence x and y');
+title({'Linear Convolution of Sequence x and y',num2str(lc)});
 ylabel('Convoluted Signal');
 
 %Circular Convolution
@@ -43,7 +43,7 @@ ylabel('Y1(n)');
 subplot(3,2,6);
 stem(cc,'filled');
 xlabel('n i.e. sample number');
-title('Circular Convolution of Sequence x1 and y1');
+title({'Circular Convolution of Sequence x1 and y1';num2str(cc)});
 ylabel('Convoluted Signal');
 
 print('convolutions','-dpng');
