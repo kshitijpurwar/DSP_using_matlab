f281c7aabbe61775ca0603e46fb4f15bbbb6d6c0
Kshtitij Purwar <kshitij.purwar4@gmail.com>
2016-04-07 17:21:12 +0530

    deleted some files and started working on lab file

--- filters/butter_pass_8.m
+++ filters/butter_pass_8.m
@@ -1,17 +1,19 @@
-[b a]=butter(7,[0.4 0.7],'bandpass');
+[b a]=butter(8,[0.4 0.7],'bandpass');
 [H,w]=freqz(b,a);
 figure(1);
 plot(w/pi,abs(H),'black','linewidth',2,'linesmoothing','on');
 axis([0 1 0 1.1]);
 grid on;
-title(['Amplitude Response of a 8th order butterworth bandpass filter with band edges at ',num2str(.4),' and ',num2str(.7)]);
+title(['Amplitude Response of a 8th order butterworth bandpass filter
+ with band edges at ',num2str(.4),' and ',num2str(.7)]);
 xlabel('Frequency (rad/sample)');
 ylabel('Gain')
 
 figure(2);
 
 plot(w,angle(H),'black','linewidth',2,'linesmoothing','on');
-title(['Amplitude Response of a 8th order butterworth bandpass filter with band edges at ',num2str(.4),' and ',num2str(.7)]);
+title(['Amplitude Response of a 8th order butterworth bandpass filter with
+ band edges at ',num2str(.4),' and ',num2str(.7)]);
 xlabel('Frequency (rad/sample)');
 ylabel('Phase (rad)');
 grid on;
