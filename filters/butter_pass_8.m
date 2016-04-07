b2bdba6e46deba70d88d6c730d6db6244b9f92bb
Kshtitij Purwar <kshitij.purwar4@gmail.com>
2016-04-08 00:12:37 +0530

    added a zero-pole plot to butterworth 8 band pass

--- filters/butter_pass_8.m
+++ filters/butter_pass_8.m
@@ -4,17 +4,21 @@ figure(1);
 plot(w/pi,abs(H),'black','linewidth',2,'linesmoothing','on');
 axis([0 1 0 1.1]);
 grid on;
-title(['Amplitude Response of a 8th order butterworth bandpass filter
- with band edges at ',num2str(.4),' and ',num2str(.7)]);
+title(['Amplitude Response of a 8th order butterworth bandpass filter with band edges at ',num2str(.4),' and ',num2str(.7)]);
 xlabel('Frequency (rad/sample)');
-ylabel('Gain')
+ylabel('Gain');
+display(['Denominator coefficients are :',num2str(round(b*100)/100)])
+
+display(['Numerator coefficients are :',num2str(round(a*100)/100)])
 
 figure(2);
 
 plot(w,angle(H),'black','linewidth',2,'linesmoothing','on');
-title(['Amplitude Response of a 8th order butterworth bandpass filter with
- band edges at ',num2str(.4),' and ',num2str(.7)]);
+title(['Amplitude Response of a 8th order butterworth bandpass filter with band edges at ',num2str(.4),' and ',num2str(.7)]);
 xlabel('Frequency (rad/sample)');
 ylabel('Phase (rad)');
 grid on;
-zplane(b,a)
+figure(3);
+zplane(b,a,'black')
+title('8th-Order Butterworth band pass Digital Filter');
+legend('zeros','poles')
